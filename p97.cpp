bd2b48798a84d4ccf907804e74a170eeea539549
Nhan Minh <solidify.ice@gmail.com>
2026-08-19 19:40:32 +0700

    after the reset

--- p97.cpp
+++ p97.cpp
@@ -1,26 +1,21 @@
 #include <bits/stdc++.h>
-
+#include <vector>
 using namespace std;
 
 int main() {
     long long y, t, x, total;
     cin >> y;
+    vector<int> differ;
     long long n[y];
     t = 0;
     for(int i = 0; i < y; i++){
         cin >> x;
         n[i] = x;
     }
-    while (t <= y - 1) {
-        if (n[t] < 0) {
-            n[t] = 0;
-        }
-        t++;
+    for (int temp = 0; temp < y; temp++) {
+        int c = count(differ.begin(), differ.end(), n[temp]);
+        if (c == 0) differ.push_back(n[temp]);
     }
-    t = 0;
-    while (t < y) {
-        cout << n[t] << " ";
-        t++;
-    }
+    cout << differ.size();
     return 0;
 }
